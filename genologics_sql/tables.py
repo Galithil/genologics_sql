6c55aac99732d8cec6f581ae7c1e110d5d049658
Denis Moreno <denis.moreno@scilifelab.se>
2015-11-16 11:49:04 +0100

    more tables

--- genologics_sql/tables.py
+++ genologics_sql/tables.py
@@ -1,8 +1,22 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy import ForeignKey, Column, Boolean, Integer, String, TIMESTAMP
+from sqlalchemy import Table, ForeignKey, Column, Boolean, Integer, Float, String, TIMESTAMP
 
 Base = declarative_base()
+
+#Junction tables first
+
+artifact_sample_map = Table('artifact_sample_map', Base.metadata,
+            Column('artifactid', Integer, ForeignKey('artifact.artifactid')),
+                Column('processid', Integer, ForeignKey('sample.processid')))
+
+artifact_ancestor_map = Table('artifact_ancestor_map', Base.metadata,
+            Column('artifactid', Integer, ForeignKey('artifact.artifactid')),
+                Column('ancestorartifactid', Integer, ForeignKey('artifact.artifactid')))
+
+
+#real tables next
+
 
 class Project(Base):
     __tablename__ = 'project'
@@ -21,6 +35,8 @@ class Project(Base):
     lastmodifiedby =    Column(Integer) 
     researcherid =      Column(Integer) 
     priority =          Column(Integer)
+
+    udfs = relationship("EntityUdfView", foreign_keys=projectid, primaryjoin="and_(Project.projectid == EntityUdfView.attachtoid, EntityUdfView.attachtoclassid == 83)")
 
     def __repr__(self):
         return "<Project(id={}, name={})>".format(self.projectid, self.name)
@@ -67,7 +83,7 @@ class ProcessType(Base):
     lastmodifieddate =  Column(TIMESTAMP)
     lastmodifiedby =    Column(Integer)
     behaviourname =     Column(String)
-    metadata =          Column(String)
+    pmetadata =         Column('metadata', String)
     canedit =           Column(Boolean)
     modulename =        Column(String)
     expertname =        Column(String)
@@ -91,17 +107,89 @@ class Process(Base):
     lastmodifiedby =    Column(Integer)
     installationid =    Column(Integer)
     techid =            Column(Integer)
-    typeid =            Column(Integer, ForeignKey('processid.typeid'))
+    typeid =            Column(Integer, ForeignKey('processtype.typeid'))
     stringparameterid = Column(Integer)
     fileparameterid =   Column(Integer)
     protocolstepid =    Column(Integer)
     workstatus =        Column(String)
     reagentcategoryid = Column(Integer)
     signedbyid =        Column(Integer)
-    signedbydate =      Column(TIMESTAMP)
+    signeddate =        Column(TIMESTAMP)
     nextstepslocked =   Column(Boolean)
 
     type = relationship(ProcessType, backref='processes')
+    udfs = relationship("ProcessUdfView")
 
     def __repr__(self):
         return "<Process(id={}, type={})>".format(self.processid, self.typeid)
+
+
+class Artifact(Base):
+    __tablename__ = 'artifact'
+    artifactid =        Column(Integer, primary_key=True)
+    name =              Column(String)
+    luid  =             Column(String)
+    volume =            Column(Float)
+    concentration =     Column(Float)
+    origvolume =        Column(Float)
+    origconcentration = Column(Float)
+    datastoreid =       Column(Integer)
+    isworking =         Column(Boolean)
+    isoriginal =        Column(String)
+    isglobal =          Column(String)
+    isgenealogyartifact=Column(String)
+    ownerid =           Column(String)
+    createddate =       Column(String)
+    lastmodifieddate =  Column(String)
+    lastmodifiedby =    Column(String)
+    artifacttypeid =    Column(String)
+    processoutputtypeid=Column(String)
+    currentstateid =    Column(String)
+    originalstateid =   Column(String)
+    compoundartifactid= Column(String)
+    outputindex =       Column(String)
+
+    samples = relationship("Sample", secondary = artifact_sample_map, backref="artifacts")
+    ancestors = relationship("Artifact", secondary = artifact_ancestor_map, 
+                primaryjoin=artifactid==artifact_ancestor_map.c.artifactid, 
+                secondaryjoin=artifactid==artifact_ancestor_map.c.ancestorartifactid)
+    udfs = relationship("ArtifactUdfView")
+    def __repr__(self):
+        return "<Artifact(id={}, name={})>".format(self.artifactid, self.name)
+
+class ArtifactUdfView(Base):
+    __tablename__ = 'artifact_udf_view'
+    artifactid =        Column(Integer, ForeignKey('artifact.artifactid') , primary_key=True)     
+    udtname =           Column(String, primary_key=True)
+    udfname =           Column(String, primary_key=True) 
+    udftype =           Column(String, primary_key=True)
+    udfvalue =          Column(String, primary_key=True)
+    udfunitlabel =      Column(String, primary_key=True)
+    def __repr__(self):
+        return "<ArtifactUdf(id={}, key={}, value={})>".format(self.artifactid, self.udfname, self.udfvalue)
+
+class ProcessUdfView(Base):
+    __tablename__ = 'process_udf_view'
+    processid =         Column(Integer, ForeignKey('process.processid') , primary_key=True)     
+    typeid =            Column(Integer, ForeignKey('processtype.typeid'), primary_key=True)
+    udtname =           Column(String, primary_key=True)
+    udfname =           Column(String, primary_key=True) 
+    udftype =           Column(String, primary_key=True)
+    udfvalue =          Column(String, primary_key=True)
+    udfunitlabel =      Column(String, primary_key=True)
+    def __repr__(self):
+        return "<ProcessUdf(id={}, key={}, value={})>".format(self.processid, self.udfname, self.udfvalue)
+
+class EntityUdfView(Base):
+    __tablename__ = 'entity_udf_view'
+    attachtoid =        Column(Integer, primary_key=True)     
+    attachtoclassid =   Column(Integer, primary_key=True)
+    udtname =           Column(String, primary_key=True)
+    udfname =           Column(String, primary_key=True) 
+    udftype =           Column(String, primary_key=True)
+    udfvalue =          Column(String, primary_key=True)
+    udfunitlabel =      Column(String, primary_key=True)
+
+
+    def __repr__(self):
+        return "<EntityUdf(id={}, class={}, key={}, value={})>".format(self.attachtoid, self.attachtoclassid, self.udfname, self.udfvalue)
